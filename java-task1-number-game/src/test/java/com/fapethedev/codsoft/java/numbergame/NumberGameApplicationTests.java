4e75904695bb6ac678ffcf05b3bcdc9b96c2e4d4
fapethedev <mpetrivlin@gmail.com>
2024-09-22 11:32:53 +0000

    Setting up the java task 1 project

---
+++ java-task1-number-game/src/test/java/com/fapethedev/codsoft/java/numbergame/NumberGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fapethedev.codsoft.java.task1_number_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task1NumberGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
