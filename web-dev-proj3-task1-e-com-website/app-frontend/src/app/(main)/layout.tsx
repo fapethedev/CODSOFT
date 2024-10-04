2cd758aec873fda6e4cfeba8f7123f858c87365c
fapethedev <mpetrivlin@gmail.com>
2024-10-04 17:10:52 +0000

    Saving the new main root layout

--- web-dev-proj3-task1-e-com-website/app-frontend/src/app/(main)/layout.tsx
+++ web-dev-proj3-task1-e-com-website/app-frontend/src/app/(main)/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "../globals.css";
 import React from "react";
+import Banner from "@/app/components/ui/banner";
+import Navigation from "@/app/components/ui/navigation";
+import Newsletter from "@/app/components/ui/newletter";
+import BlurDecor from "@/app/components/ui/blur-decor";
 
 const geistSans = localFont({
   src: "../fonts/GeistVF.woff",
@@ -41,7 +45,17 @@ export default function RootLayout({
   return (
       <html className="h-full" lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-            {children}
+            <header>
+                <Banner/>
+                <Navigation/>
+            </header>
+            <main>
+                <BlurDecor/>
+                {children}
+            </main>
+            <footer>
+                <Newsletter/>
+            </footer>
         </body>
       </html>
   );
