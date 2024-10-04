901bd1326aab267df9ebafdcc4927b41e0624d3f
fapethedev <mpetrivlin@gmail.com>
2024-10-04 13:25:50 +0000

    Setting up the springboot backend

---
+++ web-dev-proj3-task1-e-com-website/app-backend/src/main/java/com/fapethedev/codsoft/web/project3/task1/ECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.fapethedev.codsoft.web.project3.task1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+}
---
+++ web-dev-proj3-task1-e-com-website/app-backend/src/test/java/com/fapethedev/codsoft/web/project3/task1/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fapethedev.codsoft.web.project3.task1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
