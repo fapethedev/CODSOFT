5dc0a2885335344de61aea0ed22621af302107af
fapethedev <mpetrivlin@gmail.com>
2024-10-06 20:23:57 +0000

    Refactor: updates the secondary group layout.tsx

--- web-dev-proj3-task1-e-com-website/app-frontend/src/app/(secondary)/layout.tsx
+++ web-dev-proj3-task1-e-com-website/app-frontend/src/app/(secondary)/layout.tsx
@@ -1,48 +1,14 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "../globals.css";
 import React from "react";
 
-const geistSans = localFont({
-  src: "../fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-
-const geistMono = localFont({
-  src: "../fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
-export const metadata: Metadata = {
-    title: {
-        template: '%s - The Real E-Commerce',
-        default: 'The Real E-Commerce',
-    },
-    description: 'A NextJs SpringBoot E-Commerce Application Frontend',
-    icons: [
-        {
-            rel: 'icon',
-            url: 'favicon.ico'
-        },
-        {
-            rel: 'apple-touch-icon',
-            url: 'apple-icon.png',
-        }
-    ]
-};
-
-export default function RootLayout({
+export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-      <html className="h-full" lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-            {children}
-        </body>
-      </html>
+      <>
+        {children}
+      </>
   );
 }
