9a9900da1723c29f7f7a4f6385d70eca672f2d3a
fapethedev <mpetrivlin@gmail.com>
2024-10-08 17:38:18 +0000

    Add user

---
+++ web-dev-proj3-task1-e-com-website/app-backend/src/main/java/com/fapethedev/codsoft/web/project3/task1/users/User.java
@@ -0,0 +1,89 @@
+package com.fapethedev.codsoft.web.project3.task1.users;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Stream;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+    @Id
+    Long id;
+    @Column(name = "username", unique = true, nullable = false)
+    String username;
+    @Column(name = "password", unique = false, nullable = true)
+    String password;
+    @Column(name = "role")
+    Role role;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.role = Role.CLIENT;
+    }
+
+    public User(String username, String password, Role role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                '}';
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Stream.of(role)
+                .map(authorities -> new SimpleGrantedAuthority(authorities.name()))
+                .toList();
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
+
+
+
