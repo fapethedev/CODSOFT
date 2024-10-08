dee069e4a1e0c0dfe5afe0b87b4e1a901f9de716
fapethedev <mpetrivlin@gmail.com>
2024-10-08 17:38:47 +0000

    Add a repository layer for user

---
+++ web-dev-proj3-task1-e-com-website/app-backend/src/main/java/com/fapethedev/codsoft/web/project3/task1/users/UserRepository.java
@@ -0,0 +1,11 @@
+package com.fapethedev.codsoft.web.project3.task1.users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsernameIgnoreCase(String username);
+}
