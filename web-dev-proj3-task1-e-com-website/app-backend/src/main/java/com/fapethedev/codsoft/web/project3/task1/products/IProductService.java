0b276a3b26dba9fb4ca3339dec4618b77fa3e99e
fapethedev <mpetrivlin@gmail.com>
2024-10-11 19:01:31 +0000

    Enhancing the product service

--- web-dev-proj3-task1-e-com-website/app-backend/src/main/java/com/fapethedev/codsoft/web/project3/task1/products/IProductService.java
+++ web-dev-proj3-task1-e-com-website/app-backend/src/main/java/com/fapethedev/codsoft/web/project3/task1/products/IProductService.java
@@ -1,5 +1,6 @@
 package com.fapethedev.codsoft.web.project3.task1.products;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -7,7 +8,21 @@ import java.util.List;
 @Service
 public interface IProductService
 {
+    Product findById(Long id);
+
+    Product findByName(String name);
+
+    Product save(Product product);
+
+    Product update(Product product);
+
+    void delete(Product product);
+    
     List<Product> getAllProduct();
 
     List<Product> searchProducts(String name);
+
+    List<Product> searchProducts(Pageable pageable);
+    
+    List<Product> searchProducts(String name, Pageable pageable);
 }
