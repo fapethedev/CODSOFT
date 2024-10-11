e49f2a0f236aff63a1a9a9c6e77e0a3554c806a0
fapethedev <mpetrivlin@gmail.com>
2024-10-11 14:39:45 +0000

    Configure spring security

---
+++ web-dev-proj3-task1-e-com-website/app-backend/src/main/java/com/fapethedev/codsoft/web/project3/task1/config/SecurityConfiguration.java
@@ -0,0 +1,68 @@
+package com.fapethedev.codsoft.web.project3.task1.config;
+
+import com.fapethedev.codsoft.web.project3.task1.users.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.DelegatingSecurityContextRepository;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration
+{
+    private final AuthService service;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+    {
+        http.authorizeHttpRequests(
+                request -> request.requestMatchers(HttpMethod.GET, "/").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/register/", "/register", "/login", "/login/").permitAll()
+                        .anyRequest().authenticated()
+        )
+        .userDetailsService(service)
+        .httpBasic(Customizer.withDefaults())
+        .securityContext((securityContext) -> securityContext
+			.securityContextRepository(new DelegatingSecurityContextRepository(
+				new RequestAttributeSecurityContextRepository(),
+				new HttpSessionSecurityContextRepository()
+                    )
+            )
+        )
+        .csrf(AbstractHttpConfigurer::disable)
+        .rememberMe(remember -> remember.useSecureCookie(true));
+
+        return http.build();
+    }
+
+    @Bean
+	public AuthenticationManager authenticationManager(
+			UserDetailsService userDetailsService,
+			PasswordEncoder passwordEncoder) {
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setUserDetailsService(userDetailsService);
+		authenticationProvider.setPasswordEncoder(passwordEncoder);
+
+		return new ProviderManager(authenticationProvider);
+	}
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
