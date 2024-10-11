0014ba037cdd7734f7e7b2fe759157e522327438
fapethedev <mpetrivlin@gmail.com>
2024-10-11 19:12:38 +0000

    Implements the ProductService

---
+++ web-dev-proj3-task1-e-com-website/app-backend/src/main/java/com/fapethedev/codsoft/web/project3/task1/products/ProductService.java
@@ -0,0 +1,60 @@
+package com.fapethedev.codsoft.web.project3.task1.products;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class ProductService implements IProductService
+{
+    private final ProductRepository productRepository;
+
+    @Override
+    public Product findById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Product findByName(String name) {
+        return productRepository.findByName(name).orElse(null);
+    }
+
+    @Override
+    public Product save(Product color) {
+        return productRepository.save(color);
+    }
+
+    @Override
+    public Product update(Product color) {
+        return productRepository.save(color);
+    }
+
+    @Override
+    public void delete(Product color) {
+        productRepository.delete(color);
+    }
+
+    @Override
+    public List<Product> getAllProduct() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public List<Product> searchProducts(String name) {
+        return productRepository.findByNameContainingIgnoreCase(name);
+    }
+
+    @Override
+    public Page<Product> searchProducts(Pageable pageable) {
+        return productRepository.findAll(pageable);
+    }
+
+    @Override
+    public Page<Product> searchProducts(String name, Pageable pageable) {
+        return productRepository.findAllByNameContainingIgnoreCase(name, pageable);
+    }
+}
