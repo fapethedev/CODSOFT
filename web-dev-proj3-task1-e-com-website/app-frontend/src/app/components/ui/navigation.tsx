8fcd6121e969ede74d5e6bea60c1299820c9005a
fapethedev <mpetrivlin@gmail.com>
2024-10-12 16:18:12 +0000

    Update the navigation to display the account page link when there is a login user

--- web-dev-proj3-task1-e-com-website/app-frontend/src/app/components/ui/navigation.tsx
+++ web-dev-proj3-task1-e-com-website/app-frontend/src/app/components/ui/navigation.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/outline";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Dialog, DialogPanel} from "@headlessui/react";
 
 import Link from "next/link";
@@ -43,6 +43,14 @@ const navigation = [
 export default function Navigation(){
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
     const [open, setOpen] = useState(false)
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    useEffect(() => {
+        const auth = localStorage.getItem('upwdcred');
+        if (auth) {
+            setIsAuthenticated(true);
+        }
+    }, []);
 
     return (
         <>
@@ -79,9 +87,15 @@ export default function Navigation(){
                         </svg>
 
                     </Link>
-                    <Link href="/login" className="rounded-lg text-sm px-3 py-0.5 font-semibold leading-6 text-gray-900 hover:bg-gray-100">
-                        Log in <span aria-hidden="true">&rarr;</span>
-                    </Link>
+                    {isAuthenticated ? (
+                        <Link href="/account" className="rounded-lg text-sm px-3 py-0.5 font-semibold leading-6 text-gray-900 hover:bg-gray-100">
+                            Account <span aria-hidden="true">&rarr;</span>
+                        </Link>
+                    ) : (
+                        <Link href="/login" className="rounded-lg text-sm px-3 py-0.5 font-semibold leading-6 text-gray-900 hover:bg-gray-100">
+                            Log in <span aria-hidden="true">&rarr;</span>
+                        </Link>
+                    )}
                 </div>
             </nav>
             <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
@@ -125,10 +139,17 @@ export default function Navigation(){
                                               d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"/>
                                     </svg>
                                 </a>
-                                <Link href="/login"
-                                      className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-300">
-                                    Log in <span aria-hidden="true">&rarr;</span>
-                                </Link>
+                                {isAuthenticated ? (
+                                    <Link href="/account"
+                                          className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-300">
+                                        Account <span aria-hidden="true">&rarr;</span>
+                                    </Link>
+                                ) : (
+                                    <Link href="/login"
+                                          className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-300">
+                                        Log in <span aria-hidden="true">&rarr;</span>
+                                    </Link>
+                                )}
                             </div>
                         </div>
                     </div>
